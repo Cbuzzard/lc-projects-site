f498c2f414dbd52cae368736cb0555cecd550754
cbuzzard <corwin1998@gmail.com>
2020-10-20 22:35:35 -0400

    animation scripting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit{
+  
+  @ViewChild('wrapper')
+  wrapper;
+  @ViewChild('rocketContainer')
+  rocketContainer;
+  @ViewChild('assignments')
+  assignments;
+  @ViewChild('logo')
+  logo;
+  @ViewChild('logoRocket')
+  logoRocket;
+  totalframes = 94;
+  frame = 0
+  timePerFrame = 30
+
   title = 'lc-projects-site';
+
+  ngAfterContentInit(): void {
+    this.runAnimation()
+  }
+
+  runAnimation() {
+    this.frame++
+    if (this. frame === 43) this.assignments.nativeElement.classList.add('assignments-move')
+    setTimeout(()=>{
+      this.frame < this.totalframes ? this.runAnimation() : this.openLogo();
+    }, this.timePerFrame)
+  }
+
+  openLogo() {
+    this.logo.nativeElement.classList.add('logo-open')
+    this.logoRocket.nativeElement.classList.add('logo-rocket-move')
+    this.wrapper.nativeElement.classList.add('scroll')
+    this.rocketContainer.nativeElement.style.display = 'none'
+    console.log(this.assignments)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
