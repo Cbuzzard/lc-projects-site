986afc47bbfe5622211fd5b4bef83176c6fc4d14
cbuzzard <corwin1998@gmail.com>
2020-10-24 21:03:50 -0400

    formatted data

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { assignments, assignment } from '../../assignments'
 
 @Component({
   selector: 'app-assignment',
   templateUrl: './assignment.component.html',
-  styleUrls: ['./assignment.component.css']
+  styleUrls: ['./assignment.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AssignmentComponent implements OnInit {
+export class AssignmentComponent implements OnInit, AfterViewInit{
+
+  @ViewChild('cardContainer')
+  cardContainer;
+  selectedSort = 'demo';
+  assignments: Array<assignment> = [...assignments].sort((a, b) => a.demoOrder - b.demoOrder);
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    this.setAssignments()
+    this.setBackgrounds()
+  }
 
   ngOnInit(): void {
   }
 
+  setAssignments() {
+    const assignmentsTemp = assignments;
+    this.assignments = this.selectedSort === "demo" ? [...assignments].sort((a, b) => a.demoOrder - b.demoOrder) : assignments;
+  }
+
+  setBackgrounds() {
+    let i = 1;
+    for (let el of this.cardContainer.nativeElement.children) {
+      el.children[0].style.backgroundImage = `url(../../assets/galaxy_background_${i}.png)`
+      i === 5 ? i = 1 : i++;
+    }
+  }
+ 
 }
